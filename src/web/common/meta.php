bf94e7cd66f6efd405a1b7df874ec79d02b8f7e2
Nathan Wolf <nathan@elmakers.com>
2018-04-10 20:34:45 -0700

    Add special types for action and effect classes and auto-populate options for them

--- src/web/common/meta.php
+++ src/web/common/meta.php
@@ -14,7 +14,17 @@ foreach ($spellJson as $spellPredicate) {
     $texture = str_replace('item/', '', $spellPredicate['model']);
     $spellIcons['diamond_axe:' . $durability] = '<img src="common/image/' . $texture . '.png" class="spellIcon"/>' . $texture;
 }
-
 $meta->types->spell_icon->options = $spellIcons;
+
+// Populate action, effect and effectlib class types
+$actions = array_map(function($action) { return $action->short_class; }, (array)$meta->actions);
+$actions = array_fill_keys($actions, null);
+$meta->types->action_class->options = $actions;
+
+$effects = array_map(function($effect) { return $effect->short_class; }, (array)$meta->effectlib_effects);
+$effects = array_fill_keys($effects, null);
+$meta->types->effectlib_class->options = $effects;
+
+$meta->types->effect_class->options = array('EffectSingle' => null, 'EffectRing' => null, 'EffectTrail' => null);
 
 echo json_encode($meta);
