3b77bede6428047474b2bc532bb36d394cdd5a3b
Nathan Wolf <nathan@elmakers.com>
2018-04-11 17:09:10 -0700

    Change meta.php to use an associative array

--- src/web/common/meta.php
+++ src/web/common/meta.php
@@ -1,7 +1,7 @@
 <?php
 require_once('../config.inc.php');
 header('Content-Type: application/json');
-$meta = json_decode(file_get_contents('meta.json'));
+$meta = json_decode(file_get_contents('meta.json'), true);
 
 // Load resource pack textures
 $spellJson = json_decode(file_get_contents('../rp/default/assets/minecraft/models/item/diamond_axe.json'), true);
@@ -14,17 +14,17 @@ foreach ($spellJson as $spellPredicate) {
     $texture = str_replace('item/', '', $spellPredicate['model']);
     $spellIcons['diamond_axe:' . $durability] = '<img src="common/image/' . $texture . '.png" class="spellIcon"/>' . $texture;
 }
-$meta->types->spell_icon->options = $spellIcons;
+$meta['types']['spell_icon']['options'] = $spellIcons;
 
 // Populate action, effect and effectlib class types
-$actions = array_map(function($action) { return $action->short_class; }, (array)$meta->actions);
+$actions = array_column($meta['actions'], 'short_class');
 $actions = array_fill_keys($actions, null);
-$meta->types->action_class->options = $actions;
+$meta['types']['action_class']['options'] = $actions;
 
-$effects = array_map(function($effect) { return $effect->short_class; }, (array)$meta->effectlib_effects);
+$effects = array_column($meta['effectlib_effects'], 'short_class');
 $effects = array_fill_keys($effects, null);
-$meta->types->effectlib_class->options = $effects;
+$meta['types']['effectlib_class']['options'] = $effects;
 
-$meta->types->effect_class->options = array('EffectSingle' => null, 'EffectRing' => null, 'EffectTrail' => null);
+$meta['types']['effect_class']['options'] = array('EffectSingle' => null, 'EffectRing' => null, 'EffectTrail' => null);
 
 echo json_encode($meta);
